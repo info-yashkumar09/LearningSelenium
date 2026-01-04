189f6a570802509bd75e21646fdef371be183afa
Yash <info.yashkumar.09@gmail.com>
2026-01-04 19:45:44 +0530

    Xpath

---
+++ src/test/java/day03/XPath.java
@@ -0,0 +1,23 @@
+package day03;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class XPath {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://demo.nopcommerce.com/");
+        //XPath with single attribute
+        //driver.findElement(By.xpath("//input[@id='small-searchterms']")).sendKeys("Apple");
+
+        //XPath with multiple attribute
+        //driver.findElement(By.xpath("//input[@id='small-searchterms'][@placeholder='Search store']")).sendKeys("Laptop");
+
+        //XPath with 'and' 'or' operators
+        driver.findElement(By.xpath("//input[@id='small-searchterms' and @placeholder='Search store']")).sendKeys("Tablet");
+
+
+    }
+}
