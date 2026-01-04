e954b4d93b2bcb6315024fecfb2aa57bfb2257ac
Yash <info.yashkumar.09@gmail.com>
2026-01-04 20:25:29 +0530

    Xpath

--- src/test/java/day03/XPath.java
+++ src/test/java/day03/XPath.java
@@ -2,6 +2,7 @@ package day03;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class XPath {
@@ -9,6 +10,7 @@ public class XPath {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://demo.nopcommerce.com/");
+//        driver.get("https://www.amazon.in/");
         //XPath with single attribute
         //driver.findElement(By.xpath("//input[@id='small-searchterms']")).sendKeys("Apple");
 
@@ -18,7 +20,13 @@ public class XPath {
         //XPath with 'and' 'or' operators
         //driver.findElement(By.xpath("//input[@id='small-searchterms' and @placeholder='Search store']")).sendKeys("Tablet");
 
-        driver.findElement(By.xpath("//input[@id='small-searchterms' or @placeholder='Search store']")).sendKeys("Tablet");
+        //driver.findElement(By.xpath("//input[@id='small-searchterms' or @placeholder='Search store']")).sendKeys("Tablet");
+
+        //XPath with inner text
+        //driver.findElement(By.xpath("//*[text()=' Electronics ']")).click();
+
+        //boolean test =  driver.findElement(By.xpath("//h2[text()='Welcome to our store']")).isDisplayed();
+        //System.out.println(test);
 
 
     }
